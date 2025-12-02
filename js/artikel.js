f5c6855dcfd330d93be3acf04125ae740540adfe
SD NEGERI CIMAPAG <sdnegericimapag37@gmail.com>
2025-12-02 13:59:57 +0700

    Update artikel.js

--- js/artikel.js
+++ js/artikel.js
@@ -15,8 +15,8 @@ function loadArticleList() {
   fetch(`https://api.github.com/repos/${username}/${repo}/contents/${folder}`)
     .then(res => res.json())
     .then(files => {
-      const artikelFiles = files.filter(f => f.name.endsWith(".html"));
 
+      const artikelFiles = files.filter(f => f.name.endsWith(".html"));
       artikelFiles.sort((a, b) => b.name.localeCompare(a.name));
 
       listContainer.innerHTML = "";
@@ -26,12 +26,12 @@ function loadArticleList() {
         const judul = namaFile.substring(11).replace(/-/g, " ");
         const tanggal = namaFile.substring(0, 10);
 
-        // --- CARIKAN GAMBAR DENGAN NAMA SAMA ---
+        // --- cari gambar ---
         const baseName = file.name.replace(".html", "");
         const gambarJpg = files.find(f => f.name === `${baseName}.jpg`);
         const gambarPng = files.find(f => f.name === `${baseName}.png`);
 
-        let gambar = "img/no-image.jpg"; // jika tidak ada gambar
+        let gambar = "img/no-image.jpg";
 
         if (gambarJpg) {
           gambar = `https://raw.githubusercontent.com/${username}/${repo}/main/${folder}/${gambarJpg.name}`;
@@ -39,16 +39,21 @@ function loadArticleList() {
           gambar = `https://raw.githubusercontent.com/${username}/${repo}/main/${folder}/${gambarPng.name}`;
         }
 
-        // --- CARD DENGAN GAMBAR KIRI ---
+        // CARD
         listContainer.innerHTML += `
           <div class="artikel-card">
             <div class="artikel-row">
-              <img src="${gambar}" alt="${judul}" class="artikel-img" onerror="this.style.display='none'">
+
+              <div class="artikel-image-wrapper">
+                <img src="${gambar}" alt="${judul}" class="artikel-img" onerror="this.style.display='none'">
+              </div>
+
               <div class="artikel-body">
                 <h3>${judul}</h3>
                 <div class="tanggal">${tanggal}</div>
                 <button class="btn-baca" onclick="loadArticle('${file.path}')">Baca Artikel</button>
               </div>
+
             </div>
           </div>
         `;
@@ -57,7 +62,7 @@ function loadArticleList() {
 }
 
 // =====================================
-// BACA ARTIKEL DI DALAM HALAMAN
+// BACA ARTIKEL DALAM HALAMAN
 // =====================================
 function loadArticle(path) {
   fetch(`https://raw.githubusercontent.com/${username}/${repo}/main/${path}`)
